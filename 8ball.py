181b55d2639403294c7f694d611a0ba373ab30cf
gcgc321 <155697790+gcgc321@users.noreply.github.com>
2024-08-07 21:26:26 -0500

    Create 8ball.py
    8 ball game

---
+++ 8ball.py
@@ -0,0 +1,41 @@
+import random
+
+#variables
+name = "Tom"
+question = "Do you like pizza?"
+answer = ""
+random_number = random.randint(1,10)
+
+#conditional statements
+if random_number==1:
+  answer = "Yes-Definitely"
+elif random_number==2:
+  answer = "It is decidely so"
+elif random_number==3:
+  answer = "Without a doubt"
+elif random_number==4:
+  answer = "Reply hazy, try again"
+elif random_number==5:
+  answer = "Ask again later"
+elif random_number==6:
+  answer = "Better not tell you now"
+elif random_number==7:
+  answer = "My sources say no"
+elif random_number==8:
+  answer = "Outlook not so good"
+elif random_number==9:
+  answer = "Very doubtful"
+elif random_number==10:
+  answer = "As long as there isn't pineapple on it"  
+else:
+  answer = "Error"
+
+#output and conditionals for error handling
+if name == "":
+  print("Question:", question)
+else:
+   print((name), "asks:", (question))
+if question == "":
+  print(name, "please put a question.")   
+else:   
+  print("Magic 8-Ball's answer:", (answer))
