19677fefb552551d6443afb37b9bb3041a7454cb
gcgc321 <155697790+gcgc321@users.noreply.github.com>
2024-08-18 15:27:40 -0500

    Added test objects

---
+++ boredlesstourist.py
@@ -0,0 +1,9 @@
+#Recommendation Engine for where we should go
+
+# Where we are going 
+destinations = [["Paris, France"], ["Shanghai, China"], ["Los Angeles, USA"], ["Sao Paulo, Brazil"], ["Cairo, Egypt"]]
+
+#test travelers
+#This is a traveler (a user of The Boredless Tourist application) whose name is Erin Wilkes who likes historical buildings and art. 
+# Erin is in China right now, hopefully we can find some good places to show her.
+test_traveler = ["Erin WIlkes", "Shanghai, China", ["historical site", "art"]]
