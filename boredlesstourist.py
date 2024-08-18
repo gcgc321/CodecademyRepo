daa9ef44d5da64b64f40d141062a1b7a9c83938a
gcgc321 <155697790+gcgc321@users.noreply.github.com>
2024-08-18 17:08:26 -0500

    Created attractions and functionality for adding new attractions

--- boredlesstourist.py
+++ boredlesstourist.py
@@ -7,6 +7,7 @@ destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Pau
 #This is a traveler (a user of The Boredless Tourist application) whose name is Erin Wilkes who likes historical buildings and art. 
 # Erin is in China right now, hopefully we can find some good places to show her.
 test_traveler = ["Erin WIlkes", "Shanghai, China", ["historical site", "art"]]
+attractions = [[], [], [], [], []]
 
 
 #function for suggestions
@@ -27,5 +28,28 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 
-#test the index of our test traveler
-print(test_destination_index)
+
+#function for adding attractions to attraction index
+def add_attraction(destination, attraction): 
+  destination_index = get_destination_index(destination)
+  attractions[destination_index].append(attraction)
+  return attractions
+
+
+  
+add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("Sao Paulo, Brazil", ["Sao Paulo Zoo", ["zoo"]])
+add_attraction("Sao Paulo, Brazil", ["Patio do Colegio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+
+
+
+print(attractions)
